cb27bdfa8e8a2c7e593a27ec34378fa49247a10a
OverCode <avalosfigueroapedro@gmail.com>
2025-08-24 14:47:31 -0500

    style: Ajustar estructura del componente NavBar. Agregar pequeña animación de entrada y salida del menú mobile.

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -27,17 +27,15 @@ export default function NavBar() {
           <Navigation />
         </ul>
         <button onClick={handleDarkMode} className={`hidden sm:flex cursor-pointer items-center justify-center rounded-full border-2 size-10 animate-pulse-fade-in animate-duration-500 ${darkMode ? 'border-gray-400 bg-gray-400/30' : 'border-yellow-400 bg-yellow-400/30'}`}>
-          {darkMode ? <IconMoon className='text-gray-400 size-7'/> : <IconSun className='text-yellow-400 size-7'/>}
+          {darkMode ? <IconMoon className='text-gray-400 size-7' /> : <IconSun className='text-yellow-400 size-7' />}
         </button>
         <button onClick={handleMenuToggle} className='sm:hidden focus:outline-none animate-pulse-fade-in animate-duration-500'>
-          {menuOpen ? <IconX className='size-9 text-text'/> : <IconMenuDeep className='size-9 text-text'/>}
+          {menuOpen ? <IconX className='size-9 text-text' /> : <IconMenuDeep className='size-9 text-text' />}
         </button>
       </nav>
-      {menuOpen && (
-        <ul className='absolute inset-0 h-dvh w-dvw bg-background text-text flex flex-col justify-center items-center gap-10 text-xl font-semibold'>
-          <Navigation />
-        </ul>
-      )}
+      <ul className={`fixed sm:hidden top-0 right-0 bottom-0 h-dvh w-dvw bg-background text-text flex flex-col justify-center items-center gap-10 text-xl font-semibold transition-transform duration-500 ${menuOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+        <Navigation />
+      </ul>
     </>
   )
 }
