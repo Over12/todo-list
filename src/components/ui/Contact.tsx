82583b104ec5c328b2be2a5449d0f457bbac939c
OverCode <avalosfigueroapedro@gmail.com>
2025-08-25 15:30:00 -0500

    feat: Agregar funcionalidad de envío de formulario en el componente Contact

--- src/components/ui/Contact.tsx
+++ src/components/ui/Contact.tsx
@@ -1,8 +1,13 @@
 export default function Contact() {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    alert('Formulario enviado!')
+  }
+
   return (
     <section id='contacto' className='flex flex-col items-center justify-center px-10 sm:px-15 py-10'>
       <h2 className='text-2xl sm:text-3xl uppercase font-bold mb-3 sm:mb-5'>Contáctanos</h2>
-      <form action='submit' className='w-full sm:w-1/3 flex flex-col gap-4 sm:gap-5 bg-secondary/50 border-2 border-accent p-5 sm:px-8 sm:py-10 rounded-xl hover:border-primary transition-colors'>
+      <form action='submit' onSubmit={handleSubmit} className='w-full sm:w-1/3 flex flex-col gap-4 sm:gap-5 bg-secondary/50 border-2 border-accent p-5 sm:px-8 sm:py-10 rounded-xl hover:border-primary transition-colors'>
         <div className='flex flex-col gap-1'>
           <label htmlFor='nombre' className='font-semibold'>Nombre:</label>
           <input type='text' name='nombre' placeholder='Ingrese su nombre...' required className='px-2 py-1 border-b-2 border-accent focus:outline-none focus:border-primary transition-colors' />
